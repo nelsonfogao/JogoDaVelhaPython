04705ac8f2a39043cfc535fea352ccf2c2e2c4d6
nelsonbarros <nelson.barros@carambola.vc>
2023-04-03 20:28:16 -0300

    teste

--- jogodavelha.py
+++ jogodavelha.py
@@ -1,83 +1,95 @@
-branco = ''
-tabuleiro=[
-    [branco,branco,branco],
-    [branco,branco,branco],
-    [branco,branco,branco],
-    ]
-jogo = True
-jogador1 = 'x'
-jogador2 = 'o'
-jogadas = 0
-                        
-def imprimeBoard(tabuleiro):
-    print(f'{tabuleiro[0]}'+ f'\n{tabuleiro[1]}'+ f'\n{tabuleiro[2]}' )
+def VerificaTabuleiro(tabuleiro):
+    print("Tabuleiro : \n\n");
+    for i in range (0,9):
+        if((i>0) and (i%3)==0):
+            print("\n");
+        if(tabuleiro[i]==0):
+            print("- ",end=" ");
+        if (tabuleiro[i]==1):
+            print("O ",end=" ");
+        if(tabuleiro[i]==-1):    
+            print("X ",end=" ");
+    print("\n\n");
 
-def verificaJogada(jogador, jogadas):
-    jogadorLinha = int(input("escolha a linha onde quer colocar sua jogada: "))
-    if(jogadorLinha < 1 or jogadorLinha > 3):
-        print("########comando invalido, digite outro numero########")
-        return verificaJogada(jogador, jogadas)
-    jogadorColuna = int(input("escolha a coluna onde quer colocar sua jogada: "))
-    if(jogadorColuna < 1 or jogadorColuna > 3):
-        print("########comando invalido, digite outro numero########")
-        return verificaJogada(jogador, jogadas)
-    else:
-        return marcaJogada(jogadorLinha-1, jogadorColuna-1, tabuleiro, jogador, jogadas)
+
+def TurnoJogador1(tabuleiro):
+    pos=input("Digite uma posicao de [1...9]: ");
+    pos=int(pos);
+    if(tabuleiro[pos-1]!=0):
+        print("Jogada Invalida!!!");
+        exit(0) ;
+    tabuleiro[pos-1]=-1;
+
+
+def Minimax(tabuleiro,jogador):
+    x=verificaTabuleiro(tabuleiro);
+    if(x!=0):
+        return (x*jogador);
+    pos=-1;
+    valor=-2;
+    for i in range(0,9):
+        if(tabuleiro[i]==0):
+            tabuleiro[i]=jogador;
+            total=-Minimax(tabuleiro,(jogador*-1));
+            if(total>valor):
+                valor=total;
+                pos=i;
+            tabuleiro[i]=0;
+
+    if(pos==-1):
+        return 0;
+    return valor;
     
-def marcaJogada(a, b, tabuleiro, jogador, jogadas):
-    if (tabuleiro[a][b] == branco):
-        tabuleiro[a][b] = jogador
-        return True
-    else:
-        print('########jogada invalida########')    
-         
-def verificaVitoria(tabuleiro):
-    if  (tabuleiro[0][0] == tabuleiro[0][1] == tabuleiro[0][2] != branco):
-        return True
-    elif  (tabuleiro[1][0] == tabuleiro[1][1] == tabuleiro[1][2] != branco):
-        return True
-    elif  (tabuleiro[2][0] == tabuleiro[2][1] == tabuleiro[2][2] != branco):
-        return True
-    
-    elif  (tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] != branco):
-        return True
-    elif  (tabuleiro[0][2] == tabuleiro[1][1] == tabuleiro[2][0] != branco):
-        return True
-    
-    
-    elif  (tabuleiro[0][0] == tabuleiro[1][0] == tabuleiro[2][0] != branco):
-        return True
-    elif  (tabuleiro[0][1] == tabuleiro[1][1] == tabuleiro[2][1] != branco):
-        return True
-    elif  (tabuleiro[0][2] == tabuleiro[1][2] == tabuleiro[2][2] != branco):
-        return True
-    else:
-        return False
-    
-while(jogo == True):
-    imprimeBoard(tabuleiro)
-    if(jogadas %2 == 0):
-        validaJogada = verificaJogada(jogador1, jogadas)
-    else:
-        validaJogada = verificaJogada(jogador2, jogadas)
-    if(validaJogada == True):
-        jogadas = jogadas + 1
-    if(verificaVitoria(tabuleiro) == True):
-        print('---------fim de jogo---------')
-        if(jogadas %2 == 0):
-            print('**********vitoria jogador 2**********')
-            imprimeBoard(tabuleiro)
-            jogo = False
-            break
-            
+
+def TurnoPC(tabuleiro):
+    pos=-1;
+    valor=-2;
+    for i in range(0,9):
+        if(tabuleiro[i]==0):
+            tabuleiro[i]=1;
+            total=-Minimax(tabuleiro, -1);
+            tabuleiro[i]=0;
+            if(total>valor):
+                valor=total;
+                pos=i;
+ 
+    tabuleiro[pos]=1;
+
+
+
+def verificaTabuleiro(tabuleiro):
+    t=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+    for i in range(0,8):
+        if(tabuleiro[t[i][0]] != 0 and
+           tabuleiro[t[i][0]] == tabuleiro[t[i][1]] and
+           tabuleiro[t[i][0]] == tabuleiro[t[i][2]]):
+            return tabuleiro[t[i][2]];
+    return 0;
+
+def main():
+    tabuleiro=[0,0,0,0,0,0,0,0,0];
+    jogador = 1;
+    for i in range (0,9):
+        if(verificaTabuleiro(tabuleiro)!=0):
+            break;
+        if((i+jogador)%2==0):
+            TurnoPC(tabuleiro);
         else:
-            print('**********vitoria jogador 1**********')
-            imprimeBoard(tabuleiro)
-            jogo = False
-            break
-    if (jogadas >= 9):
-        print('**********deu velha**********')
-        imprimeBoard(tabuleiro)
-        jogo = False
-    
+            VerificaTabuleiro(tabuleiro);
+            TurnoJogador1(tabuleiro);
+         
 
+    x=verificaTabuleiro(tabuleiro);
+    if(x==0):
+         VerificaTabuleiro(tabuleiro);
+         print("Deu Velha!!!")
+    if(x==-1):
+         VerificaTabuleiro(tabuleiro);
+         print("X Ganhou!!!")
+    if(x==1):
+         VerificaTabuleiro(tabuleiro);
+         print("O Wins !!!!")
+       
+
+main()
